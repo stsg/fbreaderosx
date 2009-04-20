6b5645f4a8b485629d6c3714172af4b07662e6aa
sgobunov <sgobunov@c7c12182-09bc-11de-a1f0-819f45317607>
2009-04-20 12:16:56 +0000

    importing 0.10.7 version

---
+++ trunk/makefiles/config.mk
@@ -0,0 +1,60 @@
+include $(ROOTDIR)/makefiles/platforms.mk
+
+VERSION = $(shell cat $(ROOTDIR)/fbreader/VERSION)
+MAKE = make ROOTDIR=$(ROOTDIR)
+LIBMAKE = $(MAKE) ZLSHARED=$(ZLSHARED)
+
+include $(ROOTDIR)/makefiles/arch/$(TARGET_ARCH).mk
+
+ifeq "$(TARGET_ARCH)" "macosx"
+	BINDIR ?= $(INSTALLDIR)/../MacOS/
+	LIBDIR ?= $(INSTALLDIR)
+	INCDIR ?= $(INSTALLDIR)
+	SHAREDIR ?= $(INSTALLDIR)
+	BASEDIR ?= $(SHAREDIR)
+else
+	BINDIR ?= $(INSTALLDIR)/bin
+	LIBDIR ?= $(INSTALLDIR)/lib
+	INCDIR ?= $(INSTALLDIR)/include
+	SHAREDIR ?= $(INSTALLDIR)/share
+	BASEDIR ?= $(SHAREDIR)
+endif
+
+XML_LIBS ?= -lexpat
+ARCHIVER_LIBS ?= -lz -lbz2
+NETWORK_LIBS ?= -lcurl
+
+CFLAGS += -DINSTALLDIR=\"$(INSTALLDIR)\" -DBASEDIR=\"$(BASEDIR)\" -DLIBDIR=\"$(LIBDIR)\" -DIMAGEDIR=\"$(IMAGEDIR)\" -DAPPIMAGEDIR=\"$(APPIMAGEDIR)\" -DVERSION=\"$(VERSION)\"
+ifeq "$(ZLSHARED)" "yes"
+  CFLAGS += -fPIC -DZLSHARED
+endif
+
+ifeq "$(TARGET_STATUS)" "release"
+	CFLAGS += -O3
+	ifneq "$(TARGET_ARCH)" "macosx"
+			LDFLAGS += -s
+	endif
+endif
+
+ifeq "$(TARGET_STATUS)" "debug"
+	CFLAGS += -O0 -g
+endif
+ifeq "$(TARGET_STATUS)" "profile"
+	CFLAGS += -O3 -g -pg
+endif
+
+ZINCLUDE = -I $(ROOTDIR)/zlibrary/core/include -I $(ROOTDIR)/zlibrary/text/include
+
+ZLSHARED ?= yes
+
+ifeq "$(ZLSHARED)" "yes"
+  CORE_LIBS = -lm -L$(ROOTDIR)/zlibrary/core -lzlcore -ldl
+	TEXT_LIBS = -lzltext
+else
+  CORE_LIBS = -lm -L$(ROOTDIR)/zlibrary/ui -L$(ROOTDIR)/zlibrary/core -lzlcore -lzlui-$(UI_TYPE) -lzlcore $(UILIBS) $(XML_LIBS) $(ARCHIVER_LIBS) $(NETWORK_LIBS)
+	TEXT_LIBS = -lzltext $(EXTERNAL_LIBS) -llinebreak -lfribidi
+endif
+
+ifneq "$(BUILD_RESOURCE_OBJECT)" "yes"
+.resources:
+endif
